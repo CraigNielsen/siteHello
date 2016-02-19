2380f95140705f2f77431002da1c1747163c5607
CraigNielsen <craiglbn@gmail.com>
2016-02-19 13:49:09 +0200

    added pass

--- web/src/cljs/web/app.cljs
+++ web/src/cljs/web/app.cljs
@@ -2,5 +2,5 @@
 
 (defn init []
   (let [c (.. js/document (createElement "DIV"))]
-    (aset c "innerHTML" "<p>i'm dynamically created by craig</p>")
+    (aset c "innerHTML" "<p>I'm dynamically created by Craig to test deployment</p>")
     (.. js/document (getElementById "container") (appendChild c))))
