b3ae3f6e70a1634d7e881b11665d757cb256a447
CraigNielsen <craiglbn@gmail.com>
2016-02-19 15:00:35 +0200

    working?

--- web/src/cljs/web/app.cljs
+++ web/src/cljs/web/app.cljs
@@ -2,5 +2,6 @@
 
 (defn init []
   (let [c (.. js/document (createElement "DIV"))]
-    (aset c "innerHTML" "<p>I'm dynamically created by Craig to test deployment</p>")
+    (aset c "innerHTML" "<p>I'm dynamically created by Craig to test deployment.
+              this is quite something...</p>")
     (.. js/document (getElementById "container") (appendChild c))))
